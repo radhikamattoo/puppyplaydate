b902e23f3dc46f8fa981e891a1ae5c741142eaaf
Radhika Mattoo <rm3485@nyu.edu>
2017-09-08 17:28:18 -0400

    fixed authentication function

--- puppyplaydate/routes/users.js
+++ puppyplaydate/routes/users.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-function isAuthenticated(req, res, next) {
-
-    // do any checks you want to in here
-
-    // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
-    // you can do this however you want with whatever variables you set up
-    if (req.user)
-        return next();
-
-    // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
-    else res.redirect('/');
-}
+// AUTHENTICATED ROUTES
+app.use(function(req, res, next) {
+  if (req.user){
+      return next();
+  }
+  else {
+    res.redirect('/');
+  }
+});
 
 /* GET users listing. */
-router.get('/:username', isAuthenticated,function(req, res, next) {
+router.get('/:username',function(req, res, next) {
   console.log(req.user);
 
   res.render('home', {user:req.user});
