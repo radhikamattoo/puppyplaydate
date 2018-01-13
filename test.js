2302cba7023c02825902826a9343a8d4b19fad95
Radhika Mattoo <rm3485@nyu.edu>
2018-01-12 20:27:27 -0500

    fixed mocha hanging error

--- test.js
+++ test.js
@@ -2,17 +2,13 @@ const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-const host = "http://localhost:3000";
 const request  = require("supertest");
-const agent = request.agent(host)
 const app = require('./app');
 
-// const credentials = {
-//   username: 'Radhika-Mattoo',
-//   password: '10213670864262036'
-// }
-describe('Index Page', function() {
-  it('should give back status 200', function(done){
+require('dotenv').config();
+
+describe('Home Page', function() {
+  it('Should give back status 200', function(done){
   chai.request(app)
     .get('/')
     .end((err, res) => {
@@ -21,44 +17,16 @@ describe('Index Page', function() {
     });
   });
 });
-// describe('User Home Page', function() {
-//       it('should login and redirect to user page', function(done){
-//         // chai.request(host)
-//         // .post('/')
-//         // .set('Token', 'text/plain')
-//         // .set('content-type', 'application/x-www-form-urlencoded')
-//         // .type('form')
-//         // .send('grant_type=password')
-//         // .send('username=' + credentials.username)
-//         // .send('password=' + credentials.password)
-//         // .end(function(err,res){
-//         //   expect(res).to.redirectTo('http://localhost:3000/users/Radhika-Mattoo');
-//         //   done();
-//         // });
-//         agent
-//         .post('/')
-//         .field('username', credentials.username)
-//         .field('password', credentials.password)
-//         .end(function(err, res){
-//           console.log(res)
-//           expect(res).to.redirectTo('http://localhost:3000/users/Radhika-Mattoo');
-//         });
-//
-//         var agent = chai.request.agent(host)
-//         agent
-//         .post('/')
-//         .send({ username: credentials.username, password: credentials.password})
-//         .then(function (res) {
-//         expect(res).to.have.cookie('sessionid');
-//         console.log("HIII")
-//         // The `agent` now has the sessionid cookie saved, and will send it
-//         // back to the server in the next request:
-//         return agent.get('/users/Radhika-Mattoo')
-//         .then(function (res) {
-//            expect(res).to.have.status(200);
-//            done()
-//         });
-//         });
-//       });
-//
-// });
+
+describe('User Profile', function() {
+  it('Login should redirect to user page', function(done){
+    chai.request(app)
+    .post('/')
+    .send({ username: process.env.username, password: process.env.password})
+    .then(function (res) {
+    // expect(res).to.have.cookie('sessionid');
+    expect(res).to.have.status(200);
+    done();
+    });
+  });
+});
